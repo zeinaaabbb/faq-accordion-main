ea28204c619181f1e8a087fd6f1def86c26a8f34
zeinab <swarsama@hotmail.co.uk>
2024-01-16 22:35:03 +0000

    working on JS toggle

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-// set the answer to d-none (i.e not to show) in css - done
-// When the question div is hoover will turn into a brighter purple in CSS
 
 // when the question div is clicked
 // trigger the answer for that question to be opened
 // This will turn image+ into image-
 
-// document.querySelectorAll("img").forEach((img) => {
-//   img.addEventListener("click", (event) => {
-//     event.currentTarget.classList.toggle("img-circle");
-//   });
-// });
+// console.log("Hello");
+// window.alert("This is an alert");
 
-const question = document.querySelectorAll(".question");
+const buttons = document.querySelectorAll("button");
 
-// question.forEach((question)=> {
-//   quesion.addEventListener("mouseover", (event) => {
-
-// })
-
-
-
-// function myFunction() {
-//   document.getElementBy("").click();
-
-// }
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    btn.classList.toggle(".answer, .icon-minus");
+    btn.classList.add(".icon-minus");
+    btn.classList.remove("icon-plus");
+  });
+});
