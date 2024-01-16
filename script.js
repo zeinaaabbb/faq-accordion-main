8499d926b1d0a4122407bec7988d7dc9959f07b7
zeinab <swarsama@hotmail.co.uk>
2024-01-16 14:32:47 +0000

    updated the divs in html as buttons, corrected font style

---
+++ script.js
@@ -0,0 +1,26 @@
+// set the answer to d-none (i.e not to show) in css - done
+// When the question div is hoover will turn into a brighter purple in CSS
+
+// when the question div is clicked
+// trigger the answer for that question to be opened
+// This will turn image+ into image-
+
+// document.querySelectorAll("img").forEach((img) => {
+//   img.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("img-circle");
+//   });
+// });
+
+const question = document.querySelectorAll(".question");
+
+question.forEach((question)=> {
+  quesion.addEventListener("mouseover", (event) => {
+  console.log(event)
+})
+
+
+
+// function myFunction() {
+//   document.getElementBy("").click();
+
+// }
