481a65e3fd11c4354088f1b03780e1fe227205d4
zeinab <swarsama@hotmail.co.uk>
2024-01-17 12:34:50 +0000

    fixing JS issue

--- script.js
+++ script.js
@@ -8,10 +8,27 @@
 
 const buttons = document.querySelectorAll("button");
 
+function myFunction(answerDescription, plus, minus) {
+  answerDescription.style.display = "block";
+  if (plus) {
+    plus.style.display = "block";
+  }
+  if (minus) {
+    minus.style.display = "none";
+  }
+}
+
 buttons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    btn.classList.toggle(".answer, .icon-minus");
-    btn.classList.add(".icon-minus");
-    btn.classList.remove("icon-plus");
+  btn.addEventListener("click", function() {
+    const answer = btn.classList.toggle(".answer");
+    const minus = btn.classList.toggle('icon-minus');
+    const plus = btn.classList.toggle('icon-plus');
+    const answerDescription = this.nextElementSibling;
+
+    if (btn.classList.contains("icon-plus")) {
+      myFunction(answerDescription, plus, minus);
+    } else {
+      alert("error");
+    }
   });
 });
