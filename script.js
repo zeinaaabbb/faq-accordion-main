34a079e7359a6c703ded7fbc9d20eaf4a13537fd
zeinab <swarsama@hotmail.co.uk>
2024-01-17 18:18:47 +0000

    working on JS event listeners

--- script.js
+++ script.js
@@ -6,34 +6,52 @@
 // console.log("Hello");
 // window.alert("This is an alert");
 
+
 const buttons = document.querySelectorAll(".button");
-const answers = document.querySelectorAll(".answer");
-
-function opensAnswers() {
-  let answer = document.querySelector(".answer");
-  answer.style.display = "block";
-  console.log("Answer Triggered");
-  plus.style.display = "none";
-  minus.style.display = "block";
-}
+const answer = document.querySelector(".answer");
+// const expand = document.querySelectorALL(".icon-expand");
+// const reduce = document.querySelectorAll(".icon-reduce");
 
 
-buttons.forEach(btn => {
-  btn.addEventListener("click", (event) => {
-    console.log(event);
-    // event.classList.remove("answer", "icon-minus");
-    // console.log(event);
-    document.querySelector(".answer");
-    console.log(event.currentTarget);
-    btn.classList.toggle(".answer")
-    const minus = btn.classList.toggle('icon-minus')
-    const plus = btn.classList.toggle('icon-plus123')
-    const answerDescription = this.nextElementSibling
-  });
-  // if (btn.classList.contains("icon-plus")) {
-  //   console.log("btn conatins icon ")
-  //   opensAnswers();
-  // } else {
-  //   alert("error");
-  // }
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+  // console.log(event);
+  console.log(btn);
+  const answer = document.querySelector(".answer");
+  answer.classList.toggle(".icon-expand");
+  // btn.classList.toggle(".answer");
+})
 });
+
+
+
+
+
+// function opensAnswers() {
+//   let answer = document.querySelector(".answer");
+//   answer.style.display = "block";
+//   console.log("Answer Triggered");
+//   plus.style.display = "none";
+//   minus.style.display = "block";
+// }
+
+
+// buttons.forEach(btn => {
+//   btn.addEventListener("click", (event) => {
+//     console.log(event);
+//     // event.classList.remove("answer", "icon-minus");
+//     // console.log(event);
+//     document.querySelector(".answer");
+//     console.log(event.currentTarget);
+//     btn.classList.toggle(".answer")
+//     const minus = btn.classList.toggle('icon-minus')
+//     const plus = btn.classList.toggle('icon-plus123')
+//     const answerDescription = this.nextElementSibling
+//   });
+//   // if (btn.classList.contains("icon-plus")) {
+//   //   console.log("btn conatins icon ")
+//   //   opensAnswers();
+//   // } else {
+//   //   alert("error");
+//   // }
+// })
