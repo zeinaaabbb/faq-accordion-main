30959a016d0a53dec62ab740f045bee76e5228c3
zeinab <swarsama@hotmail.co.uk>
2024-01-17 13:26:09 +0000

    Uploading for test, left some console.log to remove later on

--- script.js
+++ script.js
@@ -7,28 +7,27 @@
 // window.alert("This is an alert");
 
 const buttons = document.querySelectorAll("button");
+const answers = document.querySelectorAll("answer");
 
-function myFunction(answerDescription, plus, minus) {
-  answerDescription.style.display = "block";
-  if (plus) {
-    plus.style.display = "block";
-  }
-  if (minus) {
-    minus.style.display = "none";
-  }
+function opensAnswers() {
+  answer.style.display = "block";
+  console.log("Answer Triggered");
+  plus.style.display = "none";
+  minus.style.display = "block";
 }
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", function() {
-    const answer = btn.classList.toggle(".answer");
-    const minus = btn.classList.toggle('icon-minus');
-    const plus = btn.classList.toggle('icon-plus');
-    const answerDescription = this.nextElementSibling;
 
-    if (btn.classList.contains("icon-plus")) {
-      myFunction(answerDescription, plus, minus);
-    } else {
-      alert("error");
-    }
+buttons.forEach(btn => {
+  btn.addEventListener("click", opensAnswers() {
+    btn.classList.toggle(".answer")
+    const minus = btn.classList.toggle('icon-minus')
+    const plus = btn.classList.toggle('icon-plus')
+    const answerDescription = this.nextElementSibling
   });
+  if (btn.classList.contains("icon-plus")) {
+    console.log("btn conatins icon ")
+    opensAnswers();
+  } else {
+    alert("error");
+  }
 });
